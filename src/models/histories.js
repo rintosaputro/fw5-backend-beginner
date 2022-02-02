25a35c42f8b4d46fe9df26d075843c4a7316de6f
rintosaputro <saputrorinto@gmail.com>
2022-02-02 21:01:59 +0700

    perbaikan histories

--- src/models/histories.js
+++ src/models/histories.js
@@ -1,15 +1,22 @@
 const db = require('../helpers/db');
 
+const countHistory = (data, cb) => {
+  db.query(`SELECT COUNT(*) AS total FROM histories h LEFT JOIN vehicles v ON c.id_category=v.id_category WHERE name LIKE '${data.search}%';`, (err, res) => {
+    if (err) throw err;
+    cb(res);
+  });
+};
+
 const getHistories = (cb) => {
   db.query(`SELECT id_history, h.id_user, u.name as user_name, h.id_vehicle, v.brand as brand, rent_start_date, rent_end_date, prepayment, h.createdAt, h.updatedAt 
-  FROM history h LEFT JOIN users u ON h.id_user = u.id_user LEFT JOIN vehicles v ON h.id_vehicle = v.id_vehicle;`, (err, res) => {
+  FROM histories h LEFT JOIN users u ON h.id_user = u.id_user LEFT JOIN vehicles v ON h.id_vehicle = v.id_vehicle;`, (err, res) => {
     if (err) throw err;
     cb(res);
   });
 };
 
 const checkHistory = (data, cb) => {
-  db.query(`SELECT * FROM history WHERE id_user=${data.id_user} AND id_vehicle=${data.id_vehicle} AND rent_start_date='${data.rent_start_date}' 
+  db.query(`SELECT * FROM histories WHERE id_user=${data.id_user} AND id_vehicle=${data.id_vehicle} AND rent_start_date='${data.rent_start_date}' 
   AND rent_end_date='${data.rent_end_date}' AND prepayment=${data.prepayment}`, (err, res) => {
     if (err) throw err;
     cb(res);
@@ -17,41 +24,42 @@ const checkHistory = (data, cb) => {
 };
 
 const newHistory = (cb) => {
-  db.query('SELECT * FROM history ORDER BY id_history DESC LIMIT 1', (err, res) => {
+  db.query('SELECT * FROM histories ORDER BY id_history DESC LIMIT 1', (err, res) => {
     if (err) throw err;
     cb(res);
   });
 };
 
 const addHistory = (data, cb) => {
-  db.query('INSERT INTO history SET ?', [data], (err) => {
+  db.query('INSERT INTO histories SET ?', [data], (err) => {
     if (err) throw err;
     cb();
   });
 };
 
 const getHistory = (id, cb) => {
-  db.query('SELECT * FROM history WHERE id_history=?', [id], (err, res) => {
+  db.query('SELECT * FROM histories WHERE id_history=?', [id], (err, res) => {
     if (err) throw err;
     cb(res);
   });
 };
 
 const editHistory = (data, id, cb) => {
-  db.query('UPDATE history SET ? WHERE id_history=?;', [data, id], (err, res) => {
+  db.query('UPDATE histories SET ? WHERE id_history=?;', [data, id], (err, res) => {
     if (err) throw err;
     cb(res);
   });
 };
 
 const deleteHistory = (id, cb) => {
-  db.query('DELETE FROM history WHERE id_history=?', [id], (err, res) => {
+  db.query('DELETE FROM histories WHERE id_history=?', [id], (err, res) => {
     if (err) throw err;
     cb(res);
   });
 };
 
 module.exports = {
+  countHistory,
   getHistories,
   checkHistory,
   newHistory,
