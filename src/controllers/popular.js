6247d53c59fe657bb442c378064d603936b0d12f
rintosaputro <saputrorinto@gmail.com>
2022-02-06 06:43:27 +0700

    fix: popular vehicles

--- src/controllers/popular.js
+++ src/controllers/popular.js
@@ -1,9 +1,8 @@
 const helperGet = require('../helpers/get');
 const populerModel = require('../models/popular');
-const model = require('../models/vehicles');
 
 const popular = (req, res) => {
-  helperGet(req, res, populerModel, model.countVehicle, 'popular');
+  helperGet(req, res, populerModel.popularVehicle, populerModel.countPopular, 'popular');
 };
 
 module.exports = popular;
--- src/models/popular.js
+++ src/models/popular.js
@@ -1,10 +1,17 @@
 const db = require('../helpers/db');
 
-const popularVehicle = (data, cb) => {
-  db.query(`SELECT * FROM vehicles WHERE brand LIKE '${data.search}%' ORDER by rent_count DESC LIMIT ${data.limit} OFFSET ${data.offset}`, (err, res) => {
+const countPopular = (data, cb) => {
+  db.query(`SELECT COUNT(*) as total FROM vehicles WHERE type LIKE '${data.search}%'`, (err, res) => {
     if (err) throw err;
     cb(res);
   });
 };
 
-module.exports = popularVehicle;
+const popularVehicle = (data, cb) => {
+  db.query(`SELECT * FROM vehicles WHERE type LIKE '${data.search}%' ORDER by rent_count DESC LIMIT ${data.limit} OFFSET ${data.offset}`, (err, res) => {
+    if (err) throw err;
+    cb(res);
+  });
+};
+
+module.exports = { countPopular, popularVehicle };
