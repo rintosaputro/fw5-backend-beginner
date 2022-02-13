273efc8560e2bb154c1027dc0b432519cb3c4089
rintosaputro <saputrorinto@gmail.com>
2022-02-13 19:51:03 +0700

    feat: add auth/register

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,10 +3,12 @@ const user = require('express').Router();
 const {
   login, verify, forgotRequest, confirmRegistration,
 } = require('../controllers/auth');
+const { addUser } = require('../controllers/users');
 
 user.post('/login', login);
 user.post('/verify', verify);
 user.post('/forgotPassword', forgotRequest);
+user.post('/register', addUser);
 user.post('/confrimRegistration', confirmRegistration);
 
 module.exports = user;
