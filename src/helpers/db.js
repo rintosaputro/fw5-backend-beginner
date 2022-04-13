b4a0c3d3fedb5af4710732c38706518b731da0fc
rintosaputro <saputrorinto@gmail.com>
2022-04-14 05:36:40 +0700

    feat: change db connection

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -4,17 +4,23 @@ const {
   DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
 } = process.env;
 
-const conn = mysql.createConnection({
+// const conn = mysql.createConnection({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   database: DB_NAME,
+// });
+
+// conn.connect((err) => {
+//   if (err) throw err;
+//   // eslint-disable-next-line no-console
+//   console.log('Successfully connected to database');
+// });
+const conn = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
-});
-
-conn.connect((err) => {
-  if (err) throw err;
-  // eslint-disable-next-line no-console
-  console.log('Successfully connected to database');
 });
 
 module.exports = conn;
