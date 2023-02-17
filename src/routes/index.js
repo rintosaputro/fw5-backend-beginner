9c7980d8922b198f906ecfa37525209b84b50985
rintosaputro <saputrorinto@gmail.com>
2023-02-17 11:05:14 +0700

    feat: add welcome route

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,5 +7,8 @@ route.use('/auth', require('./auth'));
 route.use('/profile', require('./profile'));
 route.use('/histories', require('./histories'));
 route.use('/categories', require('./categories'));
+route.use('/', (req, res) => {
+    res.send('Welcome to api of isi rent')
+})
 
 module.exports = route;
